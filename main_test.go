b411355697b17bc73668fe2726cd7a5fccdd482e
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-22 21:05:33 +0530

    test: add sample unit test

--- main_test.go
+++ main_test.go
@@ -1,1 +1,16 @@
 package nami_test
+
+import (
+	"testing"
+
+	"github.com/ghoshRitesh12/nami"
+)
+
+func TestGenerateRoutes(t *testing.T) {
+	// fmt.Println(filepath.Base("./something/routes/adad.go"))
+
+	nami := nami.NewRouteGenerator()
+	nami.AddMainDirPath("./routes").
+		AddRouteHandlerType("net/http", "http.HandlerFunc").
+		GenerateRoutes()
+}
