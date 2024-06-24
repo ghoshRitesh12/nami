e8f376195e2e3d348d4834d83ed97cd7d4dd0806
Ritesh Ghosh <ghoshritesh55@gmail.com>
2024-06-24 18:10:41 +0530

    test: sample testing

--- main_test.go
+++ main_test.go
@@ -1,16 +1,21 @@
-package nami_test
+package main_test
 
 import (
+	"fmt"
+	"net/http"
 	"testing"
 
-	"github.com/ghoshRitesh12/nami"
+	"github.com/ghoshRitesh12/nami/lib"
 )
 
-func TestGenerateRoutes(t *testing.T) {
-	// fmt.Println(filepath.Base("./something/routes/adad.go"))
+func TestMain(t *testing.T) {
+	flags := lib.GetParsedFlags()
+	router := http.NewServeMux()
+	router.HandleFunc("/asd", func(w http.ResponseWriter, r *http.Request) {})
 
-	nami := nami.NewRouteGenerator()
-	nami.AddMainDirPath("./routes").
-		AddRouteHandlerType("net/http", "http.HandlerFunc").
-		GenerateRoutes()
+	// routeGenerator := lib.GetRouteGenerator()
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {})
+
+	//
+	fmt.Printf("%+v\n", flags)
 }
